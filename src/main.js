71868cf3aea8491124cd9ab4d5e23b5fec346282
Tim Dévé <timdeve@users.noreply.github.com>
2018-07-19 16:45:46 +0100

    Adds changing backgrounds

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
-import App from './App.html';
+import App from './App.html'
 
 const app = new App({
 	target: document.body,
-	data: {
-		name: 'world'
-	}
-});
+})
 
-window.app = app;
+window.app = app
 
-export default app;
+export default app
